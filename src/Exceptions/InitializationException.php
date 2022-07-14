842197ad0076f569d3ff0977ae99e0cd701113d3
stephenjude <stephenjude@users.noreply.github.com>
2022-07-14 09:12:27 +0000

    Fix styling

--- src/Exceptions/InitializationException.php
+++ src/Exceptions/InitializationException.php
@@ -2,8 +2,6 @@
 
 namespace Stephenjude\PaymentGateway\Exceptions;
 
-use JetBrains\PhpStorm\Internal\LanguageLevelTypeAware;
-
 class InitializationException extends HttpException
 {
     protected $message = "payment initialization failed";
