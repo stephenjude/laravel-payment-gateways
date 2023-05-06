2e89961a3e40be51e63b65ec32b3707c547b8486
stephenjude <stephenjudesuccess@gmail.com>
2023-05-06 11:40:05 +0100

    REWORK: seerbit integration

--- src/Providers/SeerbitProvider.php
+++ src/Providers/SeerbitProvider.php
@@ -2,15 +2,13 @@
 
 namespace Stephenjude\PaymentGateway\Providers;
 
+use Illuminate\Http\Client\PendingRequest;
 use Illuminate\Support\Arr;
 use Illuminate\Support\Carbon;
 use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\Facades\Http;
 use Illuminate\Support\Str;
 use Laravel\SerializableClosure\SerializableClosure;
-use Seerbit\Client;
-use Seerbit\Service\Standard\StandardService;
-use Seerbit\Service\Status\TransactionStatusService;
 use Stephenjude\PaymentGateway\DataObjects\PaymentData;
 use Stephenjude\PaymentGateway\DataObjects\SessionData;
 use Stephenjude\PaymentGateway\Exceptions\InitializationException;
@@ -20,6 +18,11 @@ class SeerbitProvider extends AbstractProvider
 {
     public string $provider = 'seerbit';
 
+    public function http(): PendingRequest
+    {
+        return Http::withToken($this->getToken())->acceptJson();
+    }
+
     public function initializePayment(array $parameters = []): SessionData
     {
         $parameters['reference'] = 'SEBT_'.Str::random(12);
@@ -28,67 +31,65 @@ class SeerbitProvider extends AbstractProvider
 
         $parameters['session_cache_key'] = config('payment-gateways.cache.session.key').$parameters['reference'];
 
+        $seerbit = $this->initializeProvider([
+            'publicKey' => $this->publicKey,
+            'email' => Arr::get($parameters, 'email'),
+            'amount' => Arr::get($parameters, 'amount'),
+            'currency' => Arr::get($parameters, 'currency'),
+            'country' => Arr::get($parameters, 'country_code', 'NG'),
+            'paymentReference' => Arr::get($parameters, 'reference'),
+            'callbackUrl' => $parameters['callback_url'] ?? route(config('payment-gateways.routes.callback.name'), [
+                    'reference' => $parameters['reference'],
+                    'provider' => $this->provider,
+                ]),
+        ]);
+
+        $sessionData = new SessionData(
+            provider: $this->provider,
+            sessionReference: $parameters['reference'],
+            paymentReference: null,
+            checkoutSecret: null,
+            checkoutUrl: $seerbit['payments']['redirectLink'],
+            expires: $parameters['expires'],
+            closure: $parameters['closure'] ? new SerializableClosure($parameters['closure']) : null,
+        );
+
         return Cache::remember(
             key: $parameters['session_cache_key'],
             ttl: $parameters['expires'],
-            callback: function () use ($parameters) {
-                $seerbit = $this->initializeProvider([
-                    'email' => Arr::get($parameters, 'email'),
-                    'amount' => Arr::get($parameters, 'amount'),
-                    'currency' => Arr::get($parameters, 'currency'),
-                    'country' => Arr::get($parameters, 'currency'),
-                    'paymentReference' => Arr::get($parameters, 'reference'),
-                    'tokenize' => true,
-                    'callbackUrl' => $parameters['callback_url']
-                        ?? route(config('payment-gateways.routes.callback.name'), [
-                            'reference' => $parameters['reference'],
-                            'provider' => $this->provider,
-                        ]),
-                ]);
-
-                return new SessionData(
-                    provider: $this->provider,
-                    sessionReference: $parameters['reference'],
-                    paymentReference: null,
-                    checkoutSecret: null,
-                    checkoutUrl: $seerbit['payments']['redirectLink'],
-                    expires: $parameters['expires'],
-                    closure: $parameters['closure'] ? new SerializableClosure($parameters['closure']) : null,
-                );
-            }
+            callback: fn() => $sessionData
         );
     }
 
     public function confirmPayment(string $paymentReference, ?SerializableClosure $closure): PaymentData|null
     {
-        $provider = $this->verifyProvider($paymentReference);
+        $monnify = $this->verifyProvider($paymentReference);
 
-        $provider['payments'] = $provider;
-
-        $payment = new PaymentData(
-            email: $provider['data']['payments']['email'],
-            meta: ['mobile_number' => $provider['data']['payments']['mobilenumber']],
-            amount: $provider['amount'],
-            currency: 'NGN',
+        $paymentData = new PaymentData(
+            email: $monnify['customers']['customerEmail'],
+            meta: [
+                "sourceIP" => $monnify["payments"]["sourceIP"],
+                "deviceType" => $monnify["payments"]["deviceType"],
+            ],
+            amount: $monnify["payments"]['amount'],
+            currency: $monnify["payments"]['currency'],
             reference: $paymentReference,
             provider: $this->provider,
-            status: $provider['status'],
-            date: Carbon::parse($provider['transaction_date'])->toDateTimeString(),
+            status: $monnify["payments"]['gatewayMessage'],
+            date: Carbon::parse($monnify["payments"]['transactionProcessedTime'])->toDateTimeString(),
         );
 
-        if ($closure && $payment) {
-            $closure($payment);
-        }
+        $this->executeClosure($closure, $paymentData);
 
-        return $payment;
+        return $paymentData;
     }
 
     public function initializeProvider(array $parameters): mixed
     {
         try {
-            $response = (new StandardService($this->getClient()))->Initialize($parameters);
-
-            return $response->toArray()['data'];
+            return $this->http()
+                ->post($this->baseUrl.'/payments', $parameters)
+                ->json('data');
         } catch (\Exception $exception) {
             throw new InitializationException($exception->getMessage(), $exception->getCode());
         }
@@ -97,31 +98,18 @@ class SeerbitProvider extends AbstractProvider
     public function verifyProvider(string $reference): mixed
     {
         try {
-            $response = (new TransactionStatusService($this->getClient()))->ValidateTransactionStatus($reference);
-
-            return $response->toArray();
+            return $this->http()
+                ->get($this->baseUrl."/payments/query/$reference")
+                ->json('data');
         } catch (\Exception $exception) {
             throw new VerificationException($exception->getMessage(), $exception->getCode());
         }
     }
 
-    public function getClient(): Client
-    {
-        $token = $this->getToken();
-
-        $client = new Client();
-        $client->setToken($token);
-        $client->setPublicKey($this->publicKey);
-        $client->setSecretKey($this->secretKey);
-
-        return $client;
-    }
-
     private function getToken()
     {
         return Http::acceptJson()
-            ->contentType('application/json')
-            ->post($this->baseUrl.'/encrypt/keys', ['key' => "$this->secretKey.$this->publicKey"])
+            ->post($this->baseUrl.'/encrypt/keys', ['key' => $this->secretKey.'.'.$this->publicKey])
             ->json('data.EncryptedSecKey.encryptedKey');
     }
 }
